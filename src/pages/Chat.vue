74cf5e7ae760f5a5025b9b5decff982473f22946
NaomiJC <ijimenez@iesrafaelalberti.es>
2025-05-26 10:40:54 +0200

    created chats pages

---
+++ src/pages/Chat.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Chat"
+}
+</script>
+
+<template>
+
+<!--  TODO añadir implementación de los md-->
+<form>
+  <input>
+  <button>send</button>
+</form>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/ChatList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "ChatList"
+}
+</script>
+
+<template>
+  <section>
+<!--    TODO Mostrar la lista de conversaciones-->
+  </section>
+</template>
+
+<style scoped>
+
+</style>
